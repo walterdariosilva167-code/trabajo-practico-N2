dec0fe1a4f93f9642564296e4731b03f46409845
Leo Ferrer <130q>
2026-09-14 21:40:03 -0300

    Se corrige el aporte de Walter y se une la rama de Jere.

--- circulo.py
+++ circulo.py
@@ -1,19 +1,20 @@
 # Ejercicio 5 Walter
 class circulo:
-    Pi: float = 3.1416
+    PI: float = 3.1416
 
     def __init__(self, radio: float):
         self.radio = radio
+
     def establecer_radio(self, radio: float):
         self.radio = radio
 
-  #consultas
+  #Consultas
     def obtener_radio(self):
         return self.radio
     def obtener_diametro(self):
         return self.radio * 2
     def obtener_area(self):
-        return self.Pi * (self.radio ** 2)
+        return self.PI * (self.radio ** 2)
     def obtener_perimetro(self):
-        return self.Pi * self.radio
+        return 2 * self.PI * self.radio
     
--- main.py
+++ main.py
@@ -6,31 +6,34 @@ from circulo import circulo
 # TRABAJO PRACTICO NUMERO 2 - SECCIÓN B 
 # ============================================================
 
-# Ejercicio 2 Leonardo
+# Ejercicio 1 #
+print("\nEJERCICIO 1 - Se crea la clase Cancion.")
+
+# Ejercicio 2 #
 print("\nEJERCICIO 2 - Instanciar la clase Cancion 3 veces.")
 cancion1 = Cancion("LA - Bassvictim", 153, "Electronica")
 cancion2 = Cancion("KUCHUBURANKO - Plastic Tree", 324, "Rock")
 cancion3 = Cancion("Kids from the west - Feng", 110, "HipHop")
 
 
-# Ejercicio 3 Leonardo
+# Ejercicio 3 #
 print("\nEJERCICIO 3 - Géneros de las canciones")
 print(cancion1.obtenerGenero())
 print(cancion2.obtenerGenero())
 print(cancion3.obtenerGenero())
 
 
-# Ejercicio 4 Leonardo
+# Ejercicio 4 #
 print("\nEJERCICIO 4 - Modificar el género de una instancia e imprimirlo nuevamente.")
 cancion1.establecerGenero("Indie-sleaze revival, electropop y cloud rap")
 print(cancion1.obtenerGenero())
   
 
-# ejercicio 5
-print("\n EJERCICIO 5 -cuadrado lados en centimetros")
+# Ejercicio 5 # 
+print("\n EJERCICIO 5 - Se crea la clase Circulo")
 
 # ejercicio 6 #
-print("\n EJERCICIO 6 - instanciar la clase circulo 3 veces")
+print("\n EJERCICIO 6 - Instanciar la clase circulo 3 veces")
 circulo1 = circulo(5.4)
 circulo2 = circulo(15.2)
 circulo3 = circulo(30.5)
@@ -42,10 +45,22 @@ print("circulo1: ", circulo1.obtener_diametro(), "cm")
 print("circulo2: ", circulo2.obtener_diametro(),"cm")
 print("circulo3: ", circulo3.obtener_diametro(),"cm")
 
-# Ejercicio 8 Jere
+# Ejercicio 8 #
+print("\nEJERCICIO 8 - Imprimir el valor de Pi para cada instancia de Circulo")
 
+print("circulo1:", circulo1.PI)
+print("circulo2:", circulo2.PI)
+print("circulo3:", circulo3.PI)
 
-# Ejercicio 9 Jere
+# Ejercicio 9 #
+print("\nEJERCICIO 9 - Crear 2 instancias con el mismo radio y compararlas")
 
+circulo4 = circulo(20.0)
+circulo5 = circulo(20.0)
 
-# Ejercicio 10 Jere
+print(circulo4 == circulo5)
+
+# Ejercicio 10 #
+print("\nEJERCICIO 10 - Comparar los perímetros de las instancias anteriores")
+
+print(circulo4.obtener_perimetro() == circulo5.obtener_perimetro())
